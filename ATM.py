d982cf9c6fed64bda251551ff049235ea61f15ee
THE-UNKNOWN-CREATOR <70652146+THE-UNKNOWN-CREATOR@users.noreply.github.com>
2022-02-27 10:19:36 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,8 +1,13 @@
 class Atm(object):
-    def __innit__(self, atmCard, password):
+    def __init__(self, atmCard, password):
         self.balance = 20000
         self.atmCard = atmCard
         self.password = password
+        start(self)
+
+    def CheckBalance(self):
+        print(f"Your current balance is {self.balance}")
+        start(self)
         
     def Withdrawal(self):
         a = input("Input Card Number: ")
@@ -12,10 +17,14 @@ class Atm(object):
             if p == self.password:
                 am = input("Input amount to Withdraw: ")
                 self.balance -= am
+                self.CheckBalance()
+                start(self)
             else :
                 print("Invalid password")
+                start(self)
         else:
             print("Invalid Card number")
+            start(self)
 
     def Deposit(self):
         a = input("Input Card Number: ")
@@ -25,27 +34,32 @@ class Atm(object):
             if p == self.password:
                 am = input("Input amount to Deposit: ")
                 self.balance += am
+                self.CheckBalance()
+                start(self)
             else :
                 print("Invalid password")
+                start(self)
         else:
             print("Invalid Card number")
+            start(self)
 
-    def CheckBalance(self):
-        print(self.balance)
 
 def start(atm):
     print("Choose an Action: ")
     print("Check Balance - 1")
     print("Withdraw - 2")
     print("Deposit - 3")
+    print("Exit - Q")
     i = input("Enter action:  ")
 
-    if i == 1:
+    if i == "1":
         atm.CheckBalance()
-    elif i == 2:
+    elif i == "3":
         atm.Deposit()
-    elif i == 3:
+    elif i == "2":
         atm.Withdrawal()
+    elif i == "Q":
+        return
     else:
         print("invalid input")
         start(atm)
@@ -53,5 +67,5 @@ def start(atm):
 
 an = input("Input card number(Any number): ")
 pp = input("Input your password(Any password): ")
-atm = Atm()
-start(atm)
+atm = Atm(an, pp)
+#start(atm)
